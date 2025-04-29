50fba12230fe05a86782ac2ade588b0d56f7c367
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-29 19:59:02 +0600

    added Api

---
+++ API/API/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("GetAllBooks")]
+        public IActionResult GetAllBooks()
+        {
+            var books = _db.Books.ToList();
+            return Ok(books);
+        }
+    }
+}
